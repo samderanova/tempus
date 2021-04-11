92d2b60f409cb46dbe82cda6e1536face07014fc
henryg0 <hgip@uci.edu>
2021-04-10 20:13:08 -0700

    updated timer

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_picker/flutter_picker.dart';
 
 class Countdown extends StatefulWidget {
   @override
@@ -10,7 +12,14 @@ class _CountdownState extends State<Countdown> {
   bool _timerRunning = false;
   Duration _timer = new Duration(hours: 0, minutes: 0, seconds: 0);
   TextStyle _txtStyle = TextStyle(color: Colors.white);
+  TextStyle _mainTxtStyle = TextStyle(color: Colors.white, fontSize: 30);
   Timer runCode;
+  ButtonStyle btnStyle = new ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(Color(0xffEAB586)));
+  String _timeString = '00:00:00';
+  final hoursController = TextEditingController(text: '0');
+  final minutesController = TextEditingController(text: '0');
+  final secondsController = TextEditingController(text: '0');
 
   @override
   void initState() {
@@ -21,52 +30,166 @@ class _CountdownState extends State<Countdown> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          new Text("asdf"),
-          new Container(
-            alignment: Alignment.center,
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width / 2,
-            decoration: new BoxDecoration(
-              color: Colors.black,
-              shape: BoxShape.circle,
-            ),
-            child: Center(
-              child: Text(
-                this._timer.toString().substring(0, 7),
-                style: this._txtStyle,
+    return ListView(children: [
+      Container(
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              new Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: new BoxDecoration(
+                  color: Color(0xffffcccb),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    this._timeString,
+                    style: this._mainTxtStyle,
+                  ),
+                ),
               ),
-            ),
-          ),
-          new RaisedButton(
-            onPressed: () => increaseTime(),
-            child: Text('Increase timer by 1 minute'),
-          ),
-          new RaisedButton(
-            onPressed: () => startCountdown(),
-            child: Text('Start timer!'),
-          ),
-          new RaisedButton(
-            onPressed: () => stopCountdown(),
-            child: Text('Stop timer'),
-          )
-        ],
+              new SizedBox(height: 10),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  new SizedBox(width: 20),
+                  new Text('Hours: '),
+                  new Flexible(
+                    child: new TextFormField(
+                        controller: this.hoursController,
+                        decoration: new InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ]),
+                  ),
+                  new SizedBox(width: 20),
+                  new Text('Minutes: '),
+                  new Flexible(
+                    child: new TextFormField(
+                        controller: this.minutesController,
+                        decoration: new InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ]),
+                  ),
+                  new SizedBox(width: 20),
+                  new Text('Seconds: '),
+                  new Flexible(
+                    child: new TextFormField(
+                      controller: this.secondsController,
+                      decoration: new InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                    ),
+                  ),
+                  new SizedBox(width: 20),
+                ],
+              ),
+              new SizedBox(height: 10),
+              new Row(
+                children: [
+                  new Spacer(),
+                  new SizedBox(
+                    width: 125,
+                    child: new ElevatedButton(
+                      style: this.btnStyle,
+                      child: new Text("Set time"),
+                      onPressed: () => {this.setTime()},
+                    ),
+                  ),
+                  new SizedBox(width: 20),
+                  new SizedBox(
+                    width: 125,
+                    child: new ElevatedButton(
+                      style: this.btnStyle,
+                      child: new Text("Reset time"),
+                      onPressed: () => {this.resetTime()},
+                    ),
+                  ),
+                  new Spacer(),
+                ],
+              ),
+              new SizedBox(height: 10),
+              new Row(
+                children: [
+                  new Spacer(),
+                  new SizedBox(
+                    width: 125,
+                    child: new ElevatedButton(
+                        style: this.btnStyle,
+                        child: new Text('Start Timer'),
+                        onPressed: () => {
+                              this.startCountdown(),
+                            }),
+                  ),
+                  new SizedBox(width: 20),
+                  new SizedBox(
+                    width: 125,
+                    child: new ElevatedButton(
+                        style: this.btnStyle,
+                        child: new Text('Stop Timer'),
+                        onPressed: () => {
+                              this.stopCountdown(),
+                            }),
+                  ),
+                  new Spacer(),
+                ],
+              ),
+            ]),
       ),
-    );
+    ]);
   }
 
-  void increaseTime() {
+  void setTime() {
     setState(() => {
-          this._timer += new Duration(hours: 0, minutes: 1, seconds: 0),
+          this._timer = new Duration(
+            hours: int.parse(this.hoursController.text),
+            minutes: int.parse(this.minutesController.text),
+            seconds: int.parse(this.secondsController.text),
+          ),
+          this._timeString = this.getTimeString(this._timer),
         });
   }
 
+  void resetTime() {
+    setState(() => {
+          this._timer = new Duration(hours: 0, minutes: 0, seconds: 0),
+          this.hoursController.text = '0',
+          this.minutesController.text = '0',
+          this.secondsController.text = '0',
+          this._timeString = this.getTimeString(this._timer),
+        });
+  }
+
+  String getTimeString(Duration t) {
+    String res = t.toString();
+    return res.substring(0, res.length - 7);
+  }
+
   void startCountdown() {
-    if (!this._timerRunning) {
+    if (!this._timerRunning &&
+        this._timer != new Duration(hours: 0, minutes: 0, seconds: 0)) {
       setState(() => {
             this._timerRunning = true,
           });
@@ -82,9 +205,11 @@ class _CountdownState extends State<Countdown> {
   }
 
   void decreaseCountdown() {
-    if (this._timerRunning) {
+    if (this._timerRunning &&
+        this._timer != new Duration(hours: 0, minutes: 0, seconds: 0)) {
       setState(() => {
             this._timer -= new Duration(seconds: 1),
+            this._timeString = this.getTimeString(this._timer),
           });
     }
   }
