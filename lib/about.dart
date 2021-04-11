a64c87b37dcd358addfc21d8e193820bea925d82
Riley Wong <rileysw@uci.edu>
2021-04-10 19:25:52 -0700

    Added partial circle to home and about page

--- lib/about.dart
+++ lib/about.dart
@@ -3,36 +3,66 @@ import 'package:flutter/material.dart';
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Container(
-          child: Text(
-            "About",
-            style: TextStyle(
-              fontSize: 30,
-            ),
-          ),
-          decoration: new BoxDecoration(
-            color: Color(0xffffcccb),
-            shape: BoxShape.circle,
-          ),
-          height: MediaQuery.of(context).size.height / 2.5,
-          width: MediaQuery.of(context).size.width / 2.5,
+        Align(
           alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                child: Text(
+                  "About",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffffcccb),
+                  shape: BoxShape.circle,
+                ),
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: MediaQuery.of(context).size.width / 2.5,
+                alignment: Alignment.center,
+              )
+            ],
+          ),
         ),
         Container(
           alignment: Alignment.center,
           child: Padding(
             child: Text(
-              "Tempus is a motivational app that increases your focus and productivity to the max! You can create your own tasks and set timers for each one. When starting each task, the app can even lock your phone so you don't get distracted by it!",
+              "\n\n\n\nTempus is a motivational app that increases your focus and productivity to the max! You can create your own tasks and set timers for each one. When starting each task, the app can even lock your phone so you don't get distracted by it!",
               style: TextStyle(fontSize: 20, height: 1.5),
               textAlign: TextAlign.center,
             ),
-            padding: EdgeInsets.all(30),
+            padding: EdgeInsets.fromLTRB(20, 100, 20, 20),
           ),
-        )
+        ),
+        Positioned(
+          left: -100,
+          bottom: -150,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xffffcccb),
+              shape: BoxShape.circle,
+            ),
+            height: MediaQuery.of(context).size.height / 2.5,
+            width: MediaQuery.of(context).size.width / 2.5,
+          ),
+        ),
+        Positioned(
+          right: -100,
+          bottom: -150,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xffffcccb),
+              shape: BoxShape.circle,
+            ),
+            height: MediaQuery.of(context).size.height / 2.5,
+            width: MediaQuery.of(context).size.width / 2.5,
+          ),
+        ),
       ],
-      mainAxisSize: MainAxisSize.max,
     );
   }
 }
